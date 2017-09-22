8fc5714d7570f80e45b0119285d9de7695b4a823
Oh, Isaac (US - Los Angeles) <ioh@deloitte.com>
2017-09-22 14:16:44 -0700

    Commit

---
+++ dist/scripts.js
@@ -0,0 +1,197 @@
+var accessToken = "ee7ddfa17b63455190faa6859296b016",
+  baseUrl = "https://api.api.ai/v1/query?v=20170710",
+  $speechInput,
+  $recBtn,
+  recognition,
+  messageRecording = "Recording...",
+  messageCouldntHear = "I couldn't hear you, could you say that again?",
+  messageInternalError = "Oh no, there has been an internal server error",
+  messageSorry = "I'm sorry, I don't have the answer to that yet.";
+
+var predictHQSecretKey = 'ybjNxxtz5GMJdIDBKQ3isG9bXX2DYq2nEAQyc2Ig';
+var predictHQAccessToken = '6AX5dG9E5w36gFJRNsqjw2mByfX9uI';
+
+var Client = require('predicthq');
+
+var phq = new Client({access_token: predictHQAccessToken})
+
+phq.events.search({q: 'Madonna', rank_level: 5, country:'AU', 'start.gte' : '2016-01-01'})
+  .then(function(results){
+    var events = results.toArray();
+    for (var i = 0; i < events.length; i++) {
+      console.log(events[i].rank, events[i].category, events[i].title, events[i].start, events[i].location);
+    }
+  });
+
+$(document).ready(function() {
+  $speechInput = $("#speech");
+  $recBtn = $("#rec");
+
+  $speechInput.keypress(function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      send();
+    }
+  });
+
+  $recBtn.on("click", function(event) {
+    switchRecognition();
+  });
+});
+
+function startRecognition() {
+  recognition = new webkitSpeechRecognition();
+  recognition.continuous = false;
+      recognition.interimResults = false;
+
+  recognition.onstart = function(event) {
+    respond(messageRecording);
+    updateRec();
+  };
+  recognition.onresult = function(event) {
+    recognition.onend = null;
+
+    var text = "";
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        text += event.results[i][0].transcript;
+      }
+      setInput(text);
+    stopRecognition();
+  };
+  recognition.onend = function() {
+    respond(messageCouldntHear);
+    stopRecognition();
+  };
+  recognition.lang = "en-US";
+  recognition.start();
+}
+
+function stopRecognition() {
+  if (recognition) {
+    recognition.stop();
+    recognition = null;
+  }
+  updateRec();
+}
+
+function switchRecognition() {
+  if (recognition) {
+    stopRecognition();
+  } else {
+    startRecognition();
+  }
+}
+
+function setInput(text) {
+  $speechInput.val(text);
+  send();
+}
+
+function updateRec() {
+  $recBtn.text(recognition ? "Stop" : "Speak");
+}
+
+function send() {
+  var text = $speechInput.val();
+
+  var payload = {};
+
+  $.ajax({
+    type: "POST",
+    url: baseUrl,
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    headers: {
+      "Authorization": "Bearer " + accessToken
+    },
+    data: JSON.stringify({query: text, lang: "en", sessionId: "chatbot-deloitte"}),
+    success: function(data) {
+      prepareResponse(data);
+    },
+    error: function() {
+      respond(messageInternalError);
+    }
+  });
+}
+
+function prepareResponse(val) {
+  var debugJSON = JSON.stringify(val, undefined, 2);
+  var spokenResponse = val.result.speech;
+
+  if (!spokenResponse) {
+    spokenResponse = val.result.fulfillment.speech;
+
+    if (!spokenResponse) {
+      spokenResponse = val.result.fulfillment.messages[0].speech;
+    }
+  }
+
+  spokenResponse = spokenResponse + '.';
+
+  responsiveVoice.speak(spokenResponse, "US English Male", {pitch: .01}, {rate: .1});
+  // respond(spokenResponse);
+
+  // useCustomVoice(spokenResponse);
+
+  clearInput();
+}
+
+function clearInput() {
+  $('input').val('');
+}
+
+function respond(val) {
+  if (val == "") {
+    val = messageSorry;
+  }
+
+  if (val !== messageRecording) {
+    var msg = new SpeechSynthesisUtterance();
+    msg.voiceURI = "native";
+    msg.text = val;
+    msg.lang = "en-US";
+    window.speechSynthesis.speak(msg);
+  }
+
+  $("#spokenResponse").addClass("is-active").find(".spoken-response__text").html(val);
+}
+
+function useCustomVoice(spokenResponse) {
+  if ('speechSynthesis' in window) {
+    speechSynthesis.onvoiceschanged = function() {
+      var $voicelist = $('#voices');
+
+      if($voicelist.find('option').length == 0) {
+        speechSynthesis.getVoices().forEach(function(voice, index) {
+          console.log(voice);
+          var $option = $('<option>')
+          .val(index)
+          .html(voice.name + (voice.default ? ' (default)' :''));
+
+          $voicelist.append($option);
+        });
+
+        $voicelist.material_select();
+      }
+    }
+
+    var text = spokenResponse;
+    var msg = new SpeechSynthesisUtterance();
+    var voices = window.speechSynthesis.getVoices();
+    console.log(voices);
+    msg.voice = voices[57];
+    msg.rate = .8;
+    msg.pitch = 0;
+    msg.text = text;
+
+    msg.onend = function(e) {
+      console.log('Finished in ' + event.elapsedTime + ' seconds.');
+    };
+
+    console.log(speechSynthesis);
+
+    speechSynthesis.speak(msg);
+  } else {
+    $('#modal1').openModal();
+  }
+}
